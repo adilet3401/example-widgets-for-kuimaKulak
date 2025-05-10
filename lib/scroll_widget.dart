b2c8e8f9106c565c0adf3224a4aa79a05450f806
adilet <adilet.dildeev08@gmail.com>
2025-05-10 14:36:54 +0600

    l

---
+++ lib/scroll_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BooksListScreen extends StatelessWidget {
+  const BooksListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
