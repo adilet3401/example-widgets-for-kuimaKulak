1b20e8c03385a6bb2aaac860d45b5de03fb3242f
adilet <adilet.dildeev08@gmail.com>
2025-05-11 23:16:15 +0600

    gone

--- lib/audio_widget/audio_widget.dart
+++ lib/audio_widget/audio_widget.dart
@@ -33,7 +33,7 @@ class AudioBookScrollWidget extends StatelessWidget {
           // Фоновое изображение с затемнением
           SizedBox(
             width: double.infinity,
-            height: 280,
+            height: 240,
             child: Image.network(
               backgroundImageUrl,
               fit: BoxFit.cover,
@@ -42,37 +42,61 @@ class AudioBookScrollWidget extends StatelessWidget {
             ),
           ),
           // Контент поверх
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      subtitle,
+                      style: const TextStyle(
+                        color: Colors.white70,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  subtitle,
-                  style: const TextStyle(color: Colors.white70, fontSize: 16),
-                ),
-                const SizedBox(height: 16),
-                SizedBox(
-                  height: 220,
+              ),
+              // Text(
+              //   title,
+              //   style: const TextStyle(
+              //     color: Colors.white,
+              //     fontSize: 22,
+              //     fontWeight: FontWeight.bold,
+              //   ),
+              // ),
+              // Text(
+              //   subtitle,
+              //   style: const TextStyle(color: Colors.white70, fontSize: 16),
+              // ),
+              const SizedBox(height: 36),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: SizedBox(
+                  height: 230,
                   child: ListView.separated(
                     scrollDirection: Axis.horizontal,
                     itemCount: books.length,
-                    separatorBuilder: (_, __) => const SizedBox(width: 12),
+                    padding: EdgeInsets.zero,
+                    separatorBuilder: (_, __) => const SizedBox(width: 14),
                     itemBuilder: (context, index) {
                       return AudioBookCard(book: books[index]);
                     },
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ],
       ),
@@ -87,29 +111,89 @@ class AudioBookCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       width: 140,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 6,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.network(
-              book.imageUrl,
-              width: 140,
-              height: 180,
-              fit: BoxFit.cover,
+          // Обложка книги с рамкой
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(4),
+                topRight: Radius.circular(4),
+              ),
+              border: Border.all(
+                color: Colors.white, // Цвет рамки
+                width: 4, // Толщина рамки
+              ),
+            ),
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12),
+                  ),
+                  child: Image.network(
+                    book.imageUrl,
+                    width: 140,
+                    height: 146,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                // Полупрозрачный фон
+                // Positioned.fill(
+                //   child: Container(
+                //     decoration: BoxDecoration(
+                //       gradient: LinearGradient(
+                //         colors: [
+                //           Colors.black.withOpacity(0.6),
+                //           Colors.transparent,
+                //         ],
+                //         // begin: Alignment.bottomCenter,
+                //         // end: Alignment.topCenter,
+                //       ),
+                //     ),
+                //   ),
+                // ),
+              ],
             ),
           ),
-          SizedBox(height: 8),
-          Text(
-            book.title,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(),
+          const SizedBox(height: 8),
+          // Название книги
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Text(
+              book.title,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(fontSize: 11, fontWeight: FontWeight.w500),
+            ),
           ),
-          SizedBox(height: 4),
-          Text(book.author, style: TextStyle()),
+          const SizedBox(height: 4),
+          // Автор книги
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Text(
+              book.author,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(fontSize: 10, color: Colors.grey),
+            ),
+          ),
+          const SizedBox(height: 8),
         ],
       ),
     );
